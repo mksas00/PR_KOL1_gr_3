9ac9242f7e3d99d43f70384d7d3747066bb625ee
mikolaj.sas <beqs171@gmail.com>
2021-04-09 14:10:47 +0200

    kolokwium

---
+++ navbar.js
@@ -0,0 +1,4 @@
+let nav = '<nav> <h1>Nawigacja</h1>'+ '<a href="index.html">Home</a>' +' <a href="kontakt.html">Kontakt</a></nav>';
+
+let navbar = document.querySelector(".nav-bar");
+navbar.innerHTML=nav;
---
+++ script.js
@@ -0,0 +1,9 @@
+let button = document.querySelector(".button");
+let box = document.querySelector('.box')
+
+button.addEventListener('click', () => box.style.backgroundColor='#ffff00')
+
+
+let but = document.querySelector(".btn-primary");
+let area = document.querySelector(".txt_area");
+but.addEventListener('click',()=> area.style.visibility= "visible");
